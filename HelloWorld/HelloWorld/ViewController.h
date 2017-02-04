48d6ba7b28bc221f4bd644c52ade91ee9d36f31e
Yamir Godil <gydeveloper@gmail.com>
2017-02-03 22:57:31 -0800

    Add HelloWorld project to the repo

---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Yamir Godil on 1/22/17.
+//  Copyright © 2017 Godil Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Yamir Godil on 1/22/17.
+//  Copyright © 2017 Godil Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
